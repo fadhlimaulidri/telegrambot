6a149bd5e4fb46f19afc3b4158ceb4a99131388a
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2018-03-21 12:58:34 +0700

    deploy to staging

--- telegram.rb
+++ telegram.rb
@@ -9,6 +9,17 @@ headers = {'Content-Type' => 'application/json'}
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
+
+  if message.text.end_with? "/deploy"
+      bot.api.send_message(chat_id: message.chat.id, text: "Eg. /deploy staging_server staging_branch")
+  elsif message.text.split(' ').size == 3 and message.text.start_with? "/deploy"
+    a = message.text.split(" ")
+    request_type = 'post'
+    url = "http://mileaadnanhussain:dilan@qa-jenkins.vm:8080/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=#{a[2]}&staging_action=deploy&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
+    HTTParty.send(request_type, url)
+    bot.api.send_message(chat_id: message.chat.id, text: "Udah aku deploy ya #{message.from.first_name}")
+  else  
+    
     case message.text
     when 'hai','Hai','hallo','hello','Hallo','Hello'
       bot.api.send_message(chat_id: message.chat.id, text: "Hai, aku Milea. Salam kenal #{message.from.first_name}")	
@@ -32,17 +43,7 @@ Telegram::Bot::Client.run(token) do |bot|
       HTTParty.send(request_type, url, options)
       bot.api.send_message(chat_id: message.chat.id, text: "Udah aku rerun scenario yang gagal ya #{message.from.first_name}")
     when 'iya','Iya'
-      bot.api.send_message(chat_id: message.chat.id, text: "iya bacot")
-    when 'deploy'
-      qa_jenkins = 'http://mileaadnanhussain:dilan@qa-jenkins.vm:8080/job/vp-prepaid-automation/buildWithParameters?deploy_branch=true&staging_branch=v4-refactor-token-listrik&migrasi-electricity-prepaid'  
-
-      request_type='post'
-      
-      body = {:parameter =>[{:name => "deploy_branch", :value => true},{:name => "staging_branch", :value => "v4-refactor-token-listrik"},{:name => "automation_branch", :value => "migrasi-electricity-prepaid"}]}
-
-      options = { headers: headers, body: body}	
-      puts HTTParty.send(request_type, qa_jenkins, options)
-      bot.api.send_message(chat_id: message.chat.id, text: "Udah aku deploy ya #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "iya bacot")	
     when 'run smoketest'
       url          = "http://mileaadnanhussain:dilan@172.16.8.89:8080/job/smoke-testing-vp-prepaid/buildWithParameters"
 
@@ -83,5 +84,8 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Udah aku rerun smoketest scenario yang gagal vp-prepaid ya #{message.from.first_name}")
       	  
     end
+
+  end
+  #end if
   end
 end
