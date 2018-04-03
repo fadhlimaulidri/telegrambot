af93d82dff17b0003a2f5182ba55bb3a4800c741
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2018-04-03 13:44:05 +0700

    run smoketest

--- telegram.rb
+++ telegram.rb
@@ -1,8 +1,8 @@
 require "telegram/bot"
 require "dotenv/load"
 require "httparty"
-  require 'logging'
-  require './log'
+require 'logging'
+require './log'
 
 token   = ENV['TOKEN']
 domain  = "http://#{ENV['USERNAME']}:#{ENV['PASSWORD']}@#{ENV['HOST']}"
@@ -16,24 +16,24 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Eg. /deploy staging69.vm master")
   elsif message.text.split(' ').size == 2 and message.text.start_with? "/backburner:restart"
     a = message.text.split(" ")
-    url = "http://mileaadnanhussain:dilan@qa-jenkins.vm:8080/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=master&staging_action=backburner:restart&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
+    url = "http://mileaadnanhussain:dilan@#{ENV['JENKINS_STAGING_HOST']}/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=master&staging_action=backburner:restart&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
     HTTParty.post(url)
   elsif message.text.split(' ').size == 2 and message.text.start_with? "/backburner:start"
     a = message.text.split(" ")
-    url = "http://mileaadnanhussain:dilan@qa-jenkins.vm:8080/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=master&staging_action=backburner:start&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
+    url = "http://mileaadnanhussain:dilan@#{ENV['JENKINS_STAGING_HOST']}/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=master&staging_action=backburner:start&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
     HTTParty.post(url)
   elsif message.text.split(' ').size == 2 and message.text.start_with? "/backburner:stop"
     a = message.text.split(" ")
-    url = "http://mileaadnanhussain:dilan@qa-jenkins.vm:8080/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=master&staging_action=backburner:stop&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
+    url = "http://mileaadnanhussain:dilan@#{ENV['JENKINS_STAGING_HOST']}/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=master&staging_action=backburner:stop&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
     HTTParty.post(url)
   elsif message.text.split(' ').size == 2 and message.text.start_with? "/lock:release"
     a = message.text.split(" ")
-    url = "http://mileaadnanhussain:dilan@qa-jenkins.vm:8080/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=master&staging_action=lock:release&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
+    url = "http://mileaadnanhussain:dilan@#{ENV['JENKINS_STAGING_HOST']}/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=master&staging_action=lock:release&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
     HTTParty.post(url)
   elsif message.text.split(' ').size == 3 and message.text.start_with? "/deploy"
     a = message.text.split(" ")
     request_type = 'post'
-    url = "http://mileaadnanhussain:dilan@qa-jenkins.vm:8080/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=#{a[2]}&staging_action=deploy&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
+    url = "http://mileaadnanhussain:dilan@#{ENV['JENKINS_STAGING_HOST']}/job/Staging%20Deployment/buildWithParameters?staging_server=#{a[1]}&staging_user=bukalapak&staging_branch=#{a[2]}&staging_action=deploy&migrate=true&reindex=false&normalize_date=true&telegram_id=#{message.chat.id}"
     HTTParty.send(request_type, url)
   else  
     
@@ -81,26 +81,11 @@ Telegram::Bot::Client.run(token) do |bot|
       # staging_validate('staging30.vm')
       bot.api.send_message(chat_id: message.chat.id, text: "iya bacot")
     when 'run smoketest'
-      url          = "http://#{ENV['USERNAME']}:#{ENV['PASSWORD']}@#{ENV['SMOKETEST_HOST']}/job/smoke-testing-vp-prepaid/buildWithParameters"
-
-      body = {
-      	:parameter => [
-      		{:name => "action", :value =>"run"},
-      		{:name => "squad_telegram_id", :value => "103443335"},
-      		{:name => "automation_branch", :value => "master"},
-      		{:name => "branch", :value => "curl"},
-      		{:name => "user_agent", :value => "blcanary"}
-      		]
-      	}
-
-      options      = { headers: headers, body: body}
       request_type='post'
-
-      HTTParty.send(request_type, url, options)
-      	
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, udah aku run smoketest-vp-prepaid ya #{message.from.first_name}")
+      url          = "http://#{ENV['USERNAME']}:#{ENV['PASSWORD']}@#{ENV['JENKINS_SMOKETEST_HOST']}/job/smoke-testing-vp-prepaid/buildWithParameters?action=false&squad_telegram_id=103443335&automation_branch=master&branch=curl&user_agent=blcanary"
+      HTTParty.send(request_type, url)
     when 'rerun smoketest'
-      url          = "http://#{ENV['USERNAME']}:#{ENV['PASSWORD']}@#{ENV['SMOKETEST_HOST']}/job/smoke-testing-vp-prepaid/buildWithParameters"
+      url          = "http://#{ENV['USERNAME']}:#{ENV['PASSWORD']}@#{ENV['JENKINS_SMOKETEST_HOST']}/job/smoke-testing-vp-prepaid/buildWithParameters"
       
       body = {
       	:parameter => [
@@ -125,14 +110,17 @@ Telegram::Bot::Client.run(token) do |bot|
   #end if
   logchat(message.chat.id, message.from.first_name, message.from.last_name, message.from.username, message.text)
 
-  # Try catch
-
-  begin
-    
-  rescue Faraday::Error::ConnectionFailed => e
-    bot.api.send_message(chat_id: 103443335, text: "Milea sakit kak, cek service plis")
-  end
-  # End try catch
-
   end
 end
+
+# Try catch
+  begin
+    
+  rescue Telegram::Bot::Exceptions::ResponseError
+    bot.api.send_message(chat_id: 103443335, text: "Milea kena Telegram::Bot::Exceptions::ResponseError, cek service plis")
+  rescue Faraday::ConnectionFailed
+    bot.api.send_message(chat_id: 103443335, text: "Milea kena Faraday::ConnectionFailed, cek service plis")   
+  else
+    bot.api.send_message(chat_id: 103443335, text: "Milea")   
+  end
+# End try catch 
