a30be9f3543188f7c83f4687d4916615846e9943
Fadhli Maulidri <fadhlimaulidri@gmail.com>
2018-05-08 11:48:53 +0700

    thin start

--- app/controller/release_trains_controller.rb
+++ app/controller/release_trains_controller.rb
@@ -2,7 +2,12 @@
 
 class ReleaseTrainsController < ApplicationController
   post '/' do
-    params = JSON.parse(request.body.read)
-    'ok'
+    # params = JSON.parse(request.body.read)
+    # 'ok'
+  path = 'http://172.16.8.89:8080/view/vp-prepaid/api/json'
+  resp = HTTParty.get(path).body
+  response = JSON.parse(resp)
+
+  puts response['jobs']
   end
 end
